0313f3285cbddca9648471d3779bb6f3fa186c1e
Aidan Goettsch <aidangoettsch@gmail.com>
2020-09-22 17:43:22 -0400

    add requirements for command

--- src/main/kotlin/com/team4099/robot2021/commands/AidanCommand.kt
+++ src/main/kotlin/com/team4099/robot2021/commands/AidanCommand.kt
@@ -4,6 +4,10 @@ import com.team4099.robot2021.subsystems.AidanSubsystem
 import edu.wpi.first.wpilibj2.command.CommandBase
 
 class AidanCommand: CommandBase() {
+  init {
+    addRequirements(AidanSubsystem)
+  }
+
   override fun initialize() {
     AidanSubsystem.setPower(1.0)
   }
