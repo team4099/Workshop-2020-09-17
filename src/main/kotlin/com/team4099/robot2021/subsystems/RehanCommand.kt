575b1091a42c9351a1128729a7aecced59e3ee36
RehanKabir <rehankabir10@gmail.com>
2020-09-22 22:08:03 -0400

    Made a sample subsystem and command for it

---
+++ src/main/kotlin/com/team4099/robot2021/subsystems/RehanCommand.kt
@@ -0,0 +1,15 @@
+package com.team4099.robot2021.subsystems
+
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+class RehanCommand: CommandBase() {
+  init {
+    addRequirements(RehanSubsystem)
+  }
+  override fun initialize() {
+  }
+  override fun execute() {
+  }
+  override fun isFinished(): Boolean {
+  }
+}
